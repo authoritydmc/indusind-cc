dca9321c6eda710c8628e4ad321b03c316ac89ce
Raj Dubey <com.mailuser@gmail.com>
2022-02-16 19:31:37 +0530

    adding service and assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-hangeimport { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from './data-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'indusind-cc';
+cred:string='to be fetched';
+ constructor(private service:DataServiceService){
+
+ }
+  ngOnInit(){
+    
+this.service.getDBurl().subscribe(r=>{
+  this.cred=JSON.stringify(r);
+})
+   
+  }
+
 }
